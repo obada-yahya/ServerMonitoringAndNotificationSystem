7138017e9ce670edba3fd884d013314b0570ff53
Obada-Yahya <obadayahya.an@gmail.com>
2023-11-22 16:23:20 +0200

    implemented SignalRClient to receive alert messages

---
+++ EventConsumer/Program.cs
@@ -0,0 +1,11 @@
+﻿using EventConsumer.Services;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var service = new SignalRClient("http://localhost:5124/alert");
+        service.ReceiveAlertMessage();
+        Console.ReadLine();
+    }
+}
